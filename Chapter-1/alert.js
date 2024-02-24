7309cffcdcedbcda3b4052b2684695b2443a8411
MrArsalAli <159891871+MrArsalAli@users.noreply.github.com>
2024-02-24 15:15:28 +0500

    Cloned

---
+++ Chapter-1/alert.js
@@ -0,0 +1,14 @@
+// Chapter1
+var greet = "Welcome!";
+var jsLand = "Welcome to JS Land...Happy Coding!"
+var sequence = "Welcome to JS Land";
+var sequence1 = "Happy Ending";
+
+alert(greet);
+alert("Error! Please enter a valid password.")
+alert(jsLand);
+alert(sequence);
+alert(sequence1);
+
+
+
---
+++ Chapter-2/var.js
@@ -0,0 +1,34 @@
+// Chapter2
+
+
+
+// var user ="UserName";
+// var userName ="ArsalanAli";
+// var jS ="message";
+// var message = "Hello World";
+
+// var studentName = "John Doe";
+// var studentAge = "15 years old";
+// var studentQualification = "Certified Mobile Application Development";
+
+// var email ="https://www.facebook.com/profile.php?id=100066519604684";
+
+var book = "A smarter way to learn JAVASCRIPT";
+
+
+
+// alert(user);
+// alert(userName);
+// alert(jS);
+// alert(message);
+// alert(studentName);
+// alert(studentAge);
+// alert(studentQualification);
+
+
+// alert("PIZZA,PIZZ,PIZ,PI,P");
+
+
+// alert(email);
+alert(book);
+alert("I am trying to learn from the book A smarter way to learn Javascript");
