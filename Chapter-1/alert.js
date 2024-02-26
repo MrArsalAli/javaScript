4c9264f79905e71cb500d022b797ca77175d3a0a
MrArsalAli <159891871+MrArsalAli@users.noreply.github.com>
2024-02-26 19:19:51 +0500

    Updated

--- Chapter-1/alert.js
+++ Chapter-1/alert.js
@@ -1,4 +1,4 @@
-// Chapter1
+
 var greet = "Welcome!";
 var jsLand = "Welcome to JS Land...Happy Coding!"
 var sequence = "Welcome to JS Land";
--- Chapter-2/var.js
+++ Chapter-2/var.js
@@ -1,34 +1,28 @@
-// Chapter2
 
-
-
-// var user ="UserName";
-// var userName ="ArsalanAli";
-// var jS ="message";
-// var message = "Hello World";
-
-// var studentName = "John Doe";
-// var studentAge = "15 years old";
-// var studentQualification = "Certified Mobile Application Development";
-
-// var email ="https://www.facebook.com/profile.php?id=100066519604684";
-
+var user ="UserName";
+var userName ="ArsalanAli";
+var jS ="message";
+var message = "Hello World";
+var studentName = "John Doe";
+var studentAge = "15 years old";
+var studentQualification = "Certified Mobile Application Development";
+var email ="https://www.facebook.com/profile.php?id=100066519604684";
 var book = "A smarter way to learn JAVASCRIPT";
 
 
 
-// alert(user);
-// alert(userName);
-// alert(jS);
-// alert(message);
-// alert(studentName);
-// alert(studentAge);
-// alert(studentQualification);
+alert(user);
+alert(userName);
+alert(jS);
+alert(message);
+alert(studentName);
+alert(studentAge);
+alert(studentQualification);
 
 
-// alert("PIZZA,PIZZ,PIZ,PI,P");
+alert("PIZZA,PIZZ,PIZ,PI,P");
 
 
-// alert(email);
+alert(email);
 alert(book);
 alert("I am trying to learn from the book A smarter way to learn Javascript");
