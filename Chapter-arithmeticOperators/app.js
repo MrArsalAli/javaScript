3dc3a7821948722d2ad814fa2629db7e40b5cb2d
MrArsalAli <159891871+MrArsalAli@users.noreply.github.com>
2024-02-28 17:50:40 +0500

    working

--- Chapter-arithmeticOperators/app.js
+++ Chapter-arithmeticOperators/app.js
@@ -1,6 +1,77 @@
-var x = "25";
-var y = "7";
-var result1 = x%y;
 
+// q1
+var x = 25;
+var y = 7;
+var result1 = x % y;
 
 console.log(result1);
+
+//q2
+var a = 15;
+var b = 4;
+var result2 = 10 + a * b - 2;
+
+console.log(result2);
+
+//q3
+var c = 12;
+var d = 3;
+var result3 = c ** d;
+
+console.log(result3);
+
+//q4
+var e = 10;
+var f = 3;
+var result4 = e += f * 2;
+
+console.log(result4);
+
+//q5
+var g = 20;
+var h = 4;
+var result5 = g + 5 / h;
+
+console.log(result5);
+
+//q6
+var i = 8;
+var j = 3;
+var result6 = (i - 2) * j;
+
+console.log(result6);
+
+//q7
+var k = 18;
+var l = 5;
+var result7 = k - l + 2;
+
+console.log(result7);
+
+//q8
+var m = 7;
+var n = 2;
+var result8 = m ** n;
+
+console.log(result8);
+
+//q9
+var o = 13;
+var p = 4;
+var result9 = o % (p + 2);
+
+console.log(result9);
+
+//q10
+var q = 16;
+var r = 3;
+var result10 = q / 2 * r;
+
+console.log(result10);
+
+//q11
+var s = 25;
+var t = 3;
+var result11 = s - 1 / t;
+
+console.log(result11);
