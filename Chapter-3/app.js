f9e311c5269f1841c4a6bc97582310f2f567c7bc
MrArsalAli <159891871+MrArsalAli@users.noreply.github.com>
2024-02-28 16:25:16 +0500

    Worked

---
+++ Chapter-3/app.js
@@ -0,0 +1,16 @@
+var age = "I am 21 Years Old";
+var webVisited = "You have visited this site 19 times";
+var birthYear = 2002;
+var visitorsName = "Doe";
+var quantity = 5;
+var productTitle = "T-Shirts";
+
+
+
+alert(age);
+alert(webVisited);
+alert(birthYear);
+alert(visitorsName);
+alert("ordered");
+alert(quantity);
+alert(productTitle);
