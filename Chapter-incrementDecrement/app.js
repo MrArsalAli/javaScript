3d64588b181111f43f891fac1e7e52462e873370
MrArsalAli <159891871+MrArsalAli@users.noreply.github.com>
2024-03-02 15:43:24 +0500

    Assignment Done

---
+++ Chapter-incrementDecrement/app.js
@@ -0,0 +1,76 @@
+var a = 8;
+a++
+
+console.log(a);
+
+var x = 15;
+--x
+
+console.log(x);
+
+var num = 25;
+num--
+
+console.log(num);
+
+var b = 10;
+++b
+
+console.log(b);
+
+var index = 5;
+index++
+
+console.log(index);
+
+var quantity = 12;
+--quantity
+
+console.log(quantity);
+
+var counter = 18;
+counter++
+
+console.log(counter);
+
+var score = 30;
+score--
+
+console.log(score);
+
+var c = 20;
+++c 
+
+console.log(c);
+
+var counter = 8;
+counter++
+
+console.log("Concatenation Exercises")
+
+var firstName = "John";
+console.log(`${firstName} Doe`);
+
+var city = "New";
+console.log(`${city} York`);
+
+var fruit = "apple";
+console.log(`${fruit} pie`);
+
+var animal = "black";
+console.log(`${animal} bird`);
+
+console.log(`JavaScript is fun`);
+
+console.log(`Open Source Intelligence`);
+
+console.log(`Happy birthday to you`);
+
+console.log(`Good Luck ,Have Fun`);
+
+console.log(`Web Design Development`);
+
+console.log(`Artificial Neural Networks`);
+
+
+
