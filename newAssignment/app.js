353543c0150ab0b2c29052052ed81dfbb997035e
MrArsalAli <159891871+MrArsalAli@users.noreply.github.com>
2024-04-21 23:18:01 +0500

    Work done

--- newAssignment/app.js
+++ newAssignment/app.js
@@ -28,7 +28,7 @@ for (var i = 0; i < spcStr.length; i++) {
 // console.log(capt);
 
 var userName = "arsalanAli      "
-console.log(`useName After Trimming-->`, userName.trim());
+console.log(`userName After Trimming-->`, userName.trim());
 
 var lorem = "Loremmm";
 lorem = lorem.replace("mmm", "m3");
@@ -37,3 +37,41 @@ console.log(lorem);
 var ipsum = "ipsummm";
 ipsum = ipsum.replace("mmm", "m3");
 console.log(ipsum);
+
+
+var sentence = "Count the number of vowels and consonants in a given string"
+var vowelCount = 0;
+var constCount = 0;
+
+// for (let i = 0; i < sentence.length; i++) {
+//     if (sentence[i].toLowerCase() === "a" || sentence[i].toLowerCase() === "e" || sentence[i].toLowerCase() === "i" || sentence[i].toLowerCase() === "o" || sentence[i].toLowerCase() === "u") {
+//         vowelCount++;
+//     }
+//     else if (sentence[i] !== " ") {
+//         constCount++;
+//     }
+
+// }
+
+// console.log(`The Number of Vowels are ${vowelCount} & Consonant are ${constCount} `);
+
+var vowels = ["a", "e", "i", "o", "u"]
+
+for (let i = 0; i < sentence.length; i++) {
+    var vowelsFound = false;
+    for (let j = 0; j < vowels.length; j++) {
+        if (sentence[i].toLowerCase() === vowels[j]) {
+            vowelsFound = true;
+            break;
+        }
+    }
+    if (vowelsFound) vowelCount++;
+    if(!vowelsFound && sentence[i] !== " ") constCount++;
+}
+console.log(`The Number of Vowels are ${vowelCount} & Consonant are ${constCount} `);
+
+
+var sentence1 = "Count the number of words in a given string.";
+var split = sentence1.split(" ");
+console.log(split);
+console.log(`The Number of words in the string is ${split.length}`);
